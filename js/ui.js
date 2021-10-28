4d12f2fda7cb3327d55a0bcc2ef571683dd3b21e
Plush777 <sky11916@naver.com>
2021-10-28 17:10:13 +0900

    init!

---
+++ js/ui.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+    console.log(`%c 
+     __ ________   __   ____    __
+    / // / __/ /  / /  / __ \  / /
+   / _  / _// /__/ /__/ /_/ / /_/ 
+  /_//_/___/____/____/\____/ (_)  
+    `,"color:yellow")
+
+
+    console.log(`
+    　　　∧ ∧　.?　
+    |￣￣( ´Д｀)￣|
+    |＼⌒⌒⌒⌒⌒⌒＼
+    |　 ＼⌒⌒⌒⌒⌒⌒＼
+    ＼　｜⌒⌒⌒⌒⌒⌒⌒|
+    　 ＼|＿＿＿＿＿＿＿_|
+    `)
+
+    var redBox = [ 
+        'background: #fff', 
+        'border: 4px solid #000', 
+        'color: red', 
+        'font-weight: bold',
+        'font-size: 36px',
+        'padding: 10px 20px 10px 20px'
+    ].join(';'); 
+
+    var orangeBox = [ 
+        'background: #fff', 
+        'border: 4px solid #000', 
+        'color: orange', 
+        'font-weight: bold',
+        'font-size: 36px',
+        'padding: 10px 20px 10px 20px'
+    ].join(';'); 
+
+    var yellowBox = [ 
+        'background: #fff', 
+        'border: 4px solid #000', 
+        'color: yellow', 
+        'font-weight: bold',
+        'font-size: 36px',
+        'padding: 10px 20px 10px 20px'
+    ].join(';'); 
+
+    var greenBox = [ 
+        'background: #fff', 
+        'border: 4px solid #000', 
+        'color: green', 
+        'font-weight: bold',
+        'font-size: 36px',
+        'padding: 10px 20px 10px 20px'
+    ].join(';'); 
+    
+    console.log('%cL', redBox);
+    console.log('%cO', orangeBox);
+    console.log('%cV', yellowBox);
+    console.log('%cE', greenBox);
+
+    console.log(`
+    난 힘들 때 빗속에서 힙합을 춰...
+    ｀、、｀ヽ｀ヽ｀、、ヽヽ、｀、ヽ｀ヽ｀ヽヽ｀ヽ｀、｀ヽ｀、ヽ｀
+    ｀、ヽ｀ヽ｀、ヽヽ｀ヽ、ヽ｀ヽ、ヽヽ｀ヽ｀、｀｀ヽ｀ヽ、ヽ、ヽ｀ヽ
+    ｀ヽ、ヽ｀ヽ｀、ヽヽ｀｀、ヽ｀、ヽヽ ዽ ヽ｀｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀
+    ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀ヽ｀
+    `)
+
+    console.log(`
+    ⠀ ⢀⣀⣀⣀
+⠀⠀⠀⠰⡿⠿⠛⠛⠻⠿⣷
+⠀⠀⠀⠀⠀⠀⣀⣄⡀⠀⠀⠀⠀⢀⣀⣀⣤⣄⣀⡀
+⠀⠀⠀⠀⠀⢸⣿⣿⣷⠀⠀⠀⠀⠛⠛⣿⣿⣿⡛⠿⠷
+⠀⠀⠀⠀⠀⠘⠿⠿⠋⠀⠀⠀⠀⠀⠀⣿⣿⣿⠇
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁
+
+⠀⠀⠀⠀⣿⣷⣄⠀⢶⣶⣷⣶⣶⣤⣀
+⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠈⠙⠻⠗
+⠀⠀⠀⣰⣿⣿⣿⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡄
+⠀⣠⣾⣿⣿⣿⣥⣶⣶⣿⣿⣿⣿⣿⠿⠿⠛⠃
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄
+⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁
+⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁
+⠀⠀⠛⢿⣿⣿⣿⣿⣿⣿⡿⠟
+⠀⠀⠀⠀⠀⠉⠉⠉
+    `)
+})
